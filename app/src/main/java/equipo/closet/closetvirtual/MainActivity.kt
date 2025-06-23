f2da110fb47df9c16a1d0b6a94cbe7a749ed885a
SaulNeri12 <saul.neri247498@potros.itson.edu.mx>
2025-06-22 20:48:01 -0600

    Boton de filtro se muestra correctamente

--- app/src/main/java/equipo/closet/closetvirtual/MainActivity.kt
+++ app/src/main/java/equipo/closet/closetvirtual/MainActivity.kt
@@ -1,12 +1,14 @@
 package equipo.closet.closetvirtual
 
 import android.os.Bundle
+import android.widget.Toast
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.button.MaterialButton
 import equipo.closet.closetvirtual.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -14,5 +16,15 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnFilter: MaterialButton = findViewById(R.id.btn_filter)
+
+        btnFilter.setOnClickListener {
+            Toast.makeText(
+                this,
+                "Filter button click!!!",
+                Toast.LENGTH_LONG
+            ).show()
+        }
     }
 }
