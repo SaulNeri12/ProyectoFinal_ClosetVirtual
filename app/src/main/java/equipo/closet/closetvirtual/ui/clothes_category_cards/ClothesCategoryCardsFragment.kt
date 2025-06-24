fea6f0c3d093ba19b574ae16248b8aff87b29c44
SaulNeri12 <saul.neri247498@potros.itson.edu.mx>
2025-06-24 10:22:59 -0600

    Boton de Navegacion Nuevo
    se agrego un botón en la barra de navegación para acceder al fragmento para la selección de prendas

--- app/src/main/java/equipo/closet/closetvirtual/ui/clothes_category_cards/ClothesCategoryCardsFragment.kt
+++ app/src/main/java/equipo/closet/closetvirtual/ui/clothes_category_cards/ClothesCategoryCardsFragment.kt
@@ -17,26 +17,19 @@ class ClothesCategoryCardsFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-
         activity?.findViewById<View>(R.id.bottom_nav_card)?.visibility = View.VISIBLE
-
-
         return inflater.inflate(R.layout.fragment_clothes_category_cards, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-
         val btnProfile: ImageView = view.findViewById(R.id.btnProfile)
         val btnLogout: ImageView = view.findViewById(R.id.btnLogout)
 
-
         btnProfile.setOnClickListener {
-
             findNavController().navigate(R.id.action_global_to_profileFragment)
         }
-
 
         btnLogout.setOnClickListener {
             val intent = Intent(requireActivity(), LoginActivity::class.java)
