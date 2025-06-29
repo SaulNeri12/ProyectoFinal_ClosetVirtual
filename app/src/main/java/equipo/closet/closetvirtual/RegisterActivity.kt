9762946350b2cbbc5b7837b8fde8dba4dc037559
Gh0zTy1 <160164643+Gh0zTy1@users.noreply.github.com>
2025-06-28 23:42:26 -0700

    registrar usuario segun las especificaciones de neri

--- app/src/main/java/equipo/closet/closetvirtual/RegisterActivity.kt
+++ app/src/main/java/equipo/closet/closetvirtual/RegisterActivity.kt
@@ -8,9 +8,19 @@ import android.widget.EditText
 import android.widget.Spinner
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
+import equipo.closet.closetvirtual.entities.User
+import equipo.closet.closetvirtual.repositories.exceptions.RegistrationException
+import equipo.closet.closetvirtual.repositories.factories.UserRepositoryFactory
+import equipo.closet.closetvirtual.repositories.interfaces.UserRepository
+import kotlinx.coroutines.launch
+import java.text.SimpleDateFormat 
 import java.util.Calendar
+import java.util.Locale
 
 class RegisterActivity : AppCompatActivity() {
+
+    private val userRepository: UserRepository = UserRepositoryFactory.create()
 
     private lateinit var ivBack: android.widget.ImageView
     private lateinit var etName: EditText
@@ -34,20 +44,51 @@ class RegisterActivity : AppCompatActivity() {
         etConfirmPassword = findViewById(R.id.etConfirmPassword)
         btnRegister = findViewById(R.id.btnRegister)
 
-        //fill the gender spinner
         fillGenderSpinner()
-        //declare the behavior of the back button
         setBackButtonBehavior()
-        //declare the behavior of the birth date field
         setBirthDateFieldBehavior()
-        //declare the behavior of the register button
         register()
-
     }
 
-    /**
-     * Set the behavior of the birth date field
-     */
+    private fun register() {
+        btnRegister.setOnClickListener {
+            if (validateInputFields()) {
+
+                // 1. Convertir el String de la fecha a un objeto Date
+                val dateString = etBirthDate.text.toString().trim()
+                val dateFormat = SimpleDateFormat("d-M-yyyy", Locale.getDefault())
+                val birthDateObject = dateFormat.parse(dateString)!!
+
+                // 2. Crear el objeto User con los nombres y tipos correctos
+                val user = User(
+                    name = etName.text.toString().trim(),
+                    email = etMail.text.toString().trim(),
+                    birthdate = birthDateObject,
+                    gender = etGender.selectedItem.toString(),
+                    password = etPassword.text.toString()
+                )
+
+
+                lifecycleScope.launch {
+                    try {
+                        userRepository.signUp(user)
+                        Toast.makeText(this@RegisterActivity, "Registro exitoso", Toast.LENGTH_LONG).show()
+                        val intent = Intent(this@RegisterActivity, LoginActivity::class.java)
+                        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                        startActivity(intent)
+
+                    } catch (e: RegistrationException) {
+                        Toast.makeText(this@RegisterActivity, "Error en el registro: ${e.message}", Toast.LENGTH_LONG).show()
+                    } catch (e: Exception) {
+                        Toast.makeText(this@RegisterActivity, "Ocurrió un error inesperado: ${e.message}", Toast.LENGTH_LONG).show()
+                    }
+                }
+            }
+        }
+    }
+
+
+
     private fun setBirthDateFieldBehavior() {
         etBirthDate.setOnClickListener {
             val calendar = Calendar.getInstance()
@@ -59,13 +100,11 @@ class RegisterActivity : AppCompatActivity() {
                 { _, year, monthOfYear, dayOfMonth ->
                     val selectedDate = Calendar.getInstance()
                     selectedDate.set(year, monthOfYear, dayOfMonth)
-
-                    // Get current date
                     val currentDate = Calendar.getInstance()
-
                     if (selectedDate.after(currentDate)) {
-                        Toast.makeText(this, "Birth date cannot be after current date", Toast.LENGTH_SHORT).show()
+                        Toast.makeText(this, "La fecha de nacimiento no puede ser futura", Toast.LENGTH_SHORT).show()
                     } else {
+                        // El formato aquí "d-M-yyyy"
                         val dat = "$dayOfMonth-${monthOfYear + 1}-$year"
                         etBirthDate.setText(dat)
                     }
@@ -74,92 +113,40 @@ class RegisterActivity : AppCompatActivity() {
         }
     }
 
-    /**
-     * Set the behavior of the back button
-     */
     private fun setBackButtonBehavior() {
         ivBack.setOnClickListener{
             finish()
         }
     }
 
-    /**
-     * Fill the gender spinner with the gender options
-     */
     private fun fillGenderSpinner() {
-        // List of gender options
         val genderOptions = listOf("Masculino", "Femenino", "Otro")
-        // Create an ArrayAdapter using the genderOptions list
         val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, genderOptions)
-        // Set the layout resource for the dropdown menu
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-        // Set the adapter to the genderSpinner
         etGender.adapter = adapter
     }
 
-    /**
-     * Validate the input fields and return true if they are valid, false otherwise
-     */
     private fun validateInputFields(): Boolean {
-        //get the input fields
-        val name = etName.text.toString()
-        val mail = etMail.text.toString()
-        val birthDate = etBirthDate.text.toString()
-        val gender = etGender.selectedItem.toString()
-        val password = etPassword.text.toString()
-        val confirmPassword = etConfirmPassword.text.toString()
-
-        //validate empty fields
-        if (name.isEmpty()) {
-            etName.error = "Name is required"
-            Toast.makeText(this, "Name is required", Toast.LENGTH_SHORT).show()
+        if (etName.text.toString().trim().isEmpty()) {
+            etName.error = "El nombre es requerido"
             return false
         }
-        if (mail.isEmpty()) {
-            etMail.error = "Email is required"
-            Toast.makeText(this, "Email is required", Toast.LENGTH_SHORT).show()
+        if (etMail.text.toString().trim().isEmpty() || !android.util.Patterns.EMAIL_ADDRESS.matcher(etMail.text.toString().trim()).matches()) {
+            etMail.error = "Introduce un email válido"
             return false
         }
-        if (birthDate.isEmpty()) {
-            etBirthDate.error = "Birth date is required"
-            Toast.makeText(this, "Birth date is required", Toast.LENGTH_SHORT).show()
+        if (etBirthDate.text.toString().trim().isEmpty()) {
+            etBirthDate.error = "La fecha de nacimiento es requerida"
             return false
         }
-        if (gender.isEmpty()) {
-            Toast.makeText(this, "Gender is required", Toast.LENGTH_SHORT).show()
+        if (etPassword.text.toString().trim().length < 6) {
+            etPassword.error = "La contraseña debe tener al menos 6 caracteres"
             return false
         }
-        if (password.isEmpty()) {
-            etPassword.error = "Password is required"
-            Toast.makeText(this, "Password is required", Toast.LENGTH_SHORT).show()
+        if (etPassword.text.toString().trim() != etConfirmPassword.text.toString().trim()) {
+            etConfirmPassword.error = "Las contraseñas no coinciden"
             return false
         }
-        if (confirmPassword.isEmpty()) {
-            etConfirmPassword.error = "Confirm password is required"
-            Toast.makeText(this, "Confirm password is required", Toast.LENGTH_SHORT).show()
-            return false
-        }
-        if (password != confirmPassword) {
-            etConfirmPassword.error = "Passwords do not match"
-            Toast.makeText(this, "Passwords do not match", Toast.LENGTH_SHORT).show()
-            return false
-        }
-        //Default case
         return true
     }
-
-    /**
-     * Set the behavior of the register button
-     */
-    private fun register() {
-        btnRegister.setOnClickListener {
-            if (validateInputFields()) {
-                //start the login activity
-                val intent = Intent(this, LoginActivity::class.java)
-                intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
-                startActivity(intent)
-            }
-        }
-    }
-
 }
