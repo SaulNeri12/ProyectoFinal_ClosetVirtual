0807bfe8fce002e57540d1bb868ec03d4df98a0a
levin2403 <kevin.sanchez240798@potros.itson.edu.mx>
2025-07-10 19:43:55 -0700

    asdasd
    asdasd

--- app/src/main/java/equipo/closet/closetvirtual/ui/clothesSelection/ClothesSelectionViewModel.kt
+++
@@ -1,7 +0,0 @@
-package equipo.closet.closetvirtual.ui.clothesSelection
-
-import androidx.lifecycle.ViewModel
-
-class ClothesSelectionViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
-}
